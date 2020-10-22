1799860730cdb186f4677cee7e892f99898d18a5
Victor <Victortornbom@gmail.com>
2020-10-22 13:05:32 +0200

    refactor name

--- src/main/java/com/example/demo/PokemonApplication.java
+++ src/main/java/com/example/demo/PokemonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PokemonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PokemonApplication.class, args);
 	}
 
 }
