8bebce8e835f882a65f7d564ffbd26b8b8b37f8a
Victor <Victortornbom@gmail.com>
2020-10-27 13:06:16 +0100

    pokemon entity

---
+++ src/main/java/com/example/demo/entities/Pokemon.java
@@ -0,0 +1,65 @@
+package com.example.demo.entities;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+public class Pokemon {
+
+    @Id
+    private String id;
+    private String name;
+    private String height;
+    private String weight;
+    private List<String> types;
+
+    public Pokemon() {
+    }
+
+    public Pokemon(String name, String height, String weight, List<String> types) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.types = types;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+}
