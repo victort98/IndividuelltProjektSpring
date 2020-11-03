d7f85b449a02f85e8e36a38c5d4360ad8d5f8062
Victor <Victortornbom@gmail.com>
2020-11-03 16:55:16 +0100

    added User

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,87 @@
+package com.example.demo.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+
+public class User {
+
+    @Id
+    private String id;
+    @NotNull
+    private String name;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy-MM-dd", shape=JsonFormat.Shape.STRING)
+    private LocalDate birthday;
+    @NotEmpty
+    private String username;
+    @NotEmpty @Size(min = 8, max = 12)
+    private String password;
+    private List<String> roles;
+
+    public User() {
+    }
+
+    public User(@NotNull String name, LocalDate birthday, @NotEmpty String username, @NotEmpty @Size(min = 8, max = 12) String password, List<String> roles) {
+        this.name = name;
+        this.birthday = birthday;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
