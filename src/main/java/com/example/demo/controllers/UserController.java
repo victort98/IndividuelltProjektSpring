69dc803715def489ff584a580b8f0bce19b1603a
Victor <Victortornbom@gmail.com>
2020-11-03 16:58:45 +0100

    added UserController

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    @Secured("ROLE_USER")
+    public ResponseEntity<List<User>> findAllUsers(@RequestParam(required = false) String username) {
+        var users = userService.findAll(username);
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    @Secured({"ROLE_EDITOR", "ROLE_ADMIN"})
+    public ResponseEntity<User> findUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity<User> saveUser(@Validated @RequestBody User user) {
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    @PutMapping("/{id}")
+    @Secured({"ROLE_EDITOR", "ROLE_ADMIN"})
+    @ResponseStatus(HttpStatus.NO_CONTENT) // status code 204
+    public void updateUser(@PathVariable String id, @Validated @RequestBody User user) {
+        userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @Secured("ROLE_ADMIN")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // status code 204
+    public void deleteUser(@PathVariable String id) {
+        userService.delete(id);
+    }
+}
